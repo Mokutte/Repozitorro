ca43888c385cb56373d3ae7b693a1cf5c223cb12
Feroman <feromantrain@gmail.com>
2022-08-02 21:56:01 +0700

    Base search

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Provider } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { FavouritesPage } from "./pages/FavouritesPage";
+import { HomePage } from "./pages/HomePage";
+import { store } from "./store";
 
 function App() {
   return (
-    <h1 className="font-bolt">Repozitorro</h1>
+    <>
+      <Provider store={store}>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+        </Routes>
+      </Provider>
+    </>
   );
 }
 
---
+++ src/pages/FavouritesPage.tsx
@@ -0,0 +1,5 @@
+export function FavouritesPage() {
+    return (
+        <div>fab</div>
+      );
+}
